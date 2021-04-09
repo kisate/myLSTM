Fixed OneToOne gradients

check_grads.py
@@ -1,5 +1,8 @@
 from functions import *
+from model import LSTMWithOutput, ManyToOneLSTM, loss_functions
 import numpy as np
+
+np.random.seed(12321)
 
 def check_function_grad(func, h):
     shape = h.shape
@@ -11,36 +14,135 @@ def check_function_grad(func, h):
     for i in range(shape[0]):
         h_plus_eps = h.copy()
         h_minus_eps = h.copy()
-        # print(original)
-        
+
         h_plus_eps[i] += np.array(eps)
         h_minus_eps[i] += np.array(-eps)
         h_plus_loss = func(h_plus_eps)
         h_minus_loss = func(h_minus_eps)
-        # print([(self.forward(x, h_plus_eps)[0] - self.forward(x, h_minus_eps)[0]) / (2*eps)])
+
         df_dh[i] = np.array((h_plus_loss - h_minus_loss) / (2*eps))
     
     return df_dh
 
-def check_activations():
+def run_checks(checker, times, eps, name):
+    results = np.array([np.linalg.norm(checker()) for _ in range(times)])
+    result = (results < eps).all()
+    if not result:
+        print(results)
+    else:
+        print(f"{name} checks are passed")
+
+def check_square():
     n_dims = 10
 
-    # for _ in range(10):
-    #     x = np.random.rand(n_dims, 1)
-    #     y = np.random.rand(n_dims, 1)
-    #     dL_dx = square_loss_grad(x, y)
-    #     dL_dx_app = check_function_grad(lambda x: square_loss(x, y), x)
-    #     print(np.linalg.norm(dL_dx_app - dL_dx) < 1e-7)
+    x = np.random.rand(n_dims, 1)
+    y = np.random.rand(n_dims, 1)
+    dL_dx = square_loss_grad(x, y)
+    dL_dx_app = check_function_grad(lambda x: square_loss(x, y), x)
+    
+    return dL_dx - dL_dx_app
 
-    for _ in range(10):
-        x = np.random.rand(n_dims, 1)
-        y = np.zeros((n_dims, 1))
-        y[np.random.randint(10)] = np.array([1])
-        dL_dx = softmax_cross_entropy_loss_grad(x, y)
-        dL_dx_app = check_function_grad(lambda x: softmax_cross_entropy_loss(x, y), x)
-        # print(dL_dx_app)
-        # print(dL_dx)
-        print(np.linalg.norm(dL_dx_app - dL_dx) < 1e-7)
+def check_softmax_ce():
+    n_dims = 10
+    x = np.random.rand(n_dims, 1)
+    y = np.zeros((n_dims, 1))
+    y[np.random.randint(10)] = np.array([1])
+    dL_dx = softmax_cross_entropy_loss_grad(x, y)
+    dL_dx_app = check_function_grad(lambda x: softmax_cross_entropy_loss(x, y), x)
+    
+    return dL_dx_app - dL_dx
 
 
-check_activations()
+
+def check_linear_with_activation():
+    n_dims_in = 7
+    n_dims_out = 10
+
+    x = np.random.rand(n_dims_in, 1)
+    y = np.zeros((n_dims_out, 1))
+    y[np.random.randint(n_dims_out)] = np.array([1])
+
+    W1 = np.random.rand(n_dims_out, n_dims_in)
+    b1 = np.random.rand(n_dims_out, 1)
+    W2 = np.random.rand(n_dims_out, n_dims_out)
+    b2 = np.random.rand(n_dims_out, 1)
+    l = np.dot(W1, x) + b1
+    l = np.dot(W2, l) + b2
+    dL_dx = softmax_cross_entropy_loss_grad(l, y)
+    
+    dL_dx = np.dot(W2.T, dL_dx)
+    dL_dx = np.dot(W1.T, dL_dx)
+    dL_dx_app = check_function_grad(lambda x: softmax_cross_entropy_loss(np.dot(W2, np.dot(W1, x) + b1) + b2 , y), x)
+    return dL_dx_app - dL_dx
+
+
+class OneToOneLSTM(LSTMWithOutput):
+    def forward(self, inp, h=None, c=None):
+        if c is None:
+            c = np.zeros((self.n_dims_hidden, 1))
+        if h is None:
+            h = np.zeros((self.n_dims_hidden, 1))
+
+        c, h = self.forward_step(inp, c, h)
+
+        y = self.linear_output(h)
+
+        return y
+
+
+def check_one_layer():
+    n_dims_in = 7
+    n_dims_hidden = 9
+    n_dims_out = 4
+    model = OneToOneLSTM(n_dims_in, n_dims_hidden, "softmax_ce", n_dims_out, softmax)
+    
+    x = np.random.rand(n_dims_in, 1)
+    h = np.random.rand(n_dims_hidden, 1)
+    c = np.random.rand(n_dims_hidden , 1)
+    y = np.zeros((n_dims_out, 1))
+    y[np.random.randint(n_dims_out)] = np.array([1])
+
+    model.initialize_gradients()
+    model.enable_caching = False
+
+    dL_dh_app = check_function_grad(lambda h: cross_entropy_loss(model.forward(x, h, c), y), h)
+
+    model.enable_caching = True
+    model.forward(x, h, c)
+
+    dL_dh = model.backprop_output(y)
+    dL_dh = model.backprop(dL_dh, 0)
+
+    return dL_dh_app - dL_dh
+
+def check_many_to_one():
+    n_dims_in = 7
+    n_dims_hidden = 9
+    n_dims_out = 4
+    n_samples = 20
+    model = ManyToOneLSTM(n_dims_in, n_dims_hidden, "softmax_ce", n_dims_out, softmax)
+    x = np.random.rand(n_samples, n_dims_in, 1)
+    x = np.random.rand(n_dims_in, 1)
+    h = np.random.rand(n_dims_hidden, 1)
+    c = np.random.rand(n_dims_hidden , 1)
+    y = np.zeros((n_dims_out, 1))
+    y[np.random.randint(n_dims_out)] = np.array([1])
+
+    model.initialize_gradients()
+    model.enable_caching = False
+
+    dL_dh_app = check_function_grad(lambda h: cross_entropy_loss(model.forward(x, h, c), y), h)
+
+    model.enable_caching = True
+    model.forward(x, h, c)
+
+    dL_dh = model.backprop_output(y)
+    dL_dh = model.backprop(dL_dh, 0)
+
+    return dL_dh_app - dL_dh
+
+
+# check_one_layer()
+run_checks(check_many_to_one, 10, 1e-7, "Many to One")
+# check_activations()
+# check_linear_with_activation()

functions.py
@@ -22,14 +22,15 @@ def softmax(z):
 
 def softmax_cross_entropy_loss(z, y):
     e_z = np.exp(z - np.max(z))
-    # if (np.sum(- np.log (np.sum(e_z*y, axis=0) / e_z.sum(axis=0)) * y, axis=0) == np.nan).any():
-    # print((np.sum(e_z*y, axis=0) / e_z.sum(axis=0)) * y)
     return np.sum(- np.ma.log (np.sum(e_z*y, axis=0) / e_z.sum(axis=0)).filled(0) * y, axis=0)
 
 def softmax_cross_entropy_loss_grad(z, y):
     e_z = np.exp(z - np.max(z))
     s = e_z.sum(axis=0)
     return y * (np.sum(e_z*y, axis=0) / s - 1) + e_z*(1 - y) / s
+
+def cross_entropy_loss(z, y):
+    return -np.sum(np.ma.log(z*y).filled(0) * y, axis=0)
 
 def square_loss(z, y):
     return np.sum(0.5 * np.square(y - z), axis=0)

model.py
@@ -67,16 +67,16 @@ class BaseLSTM:
 
     def backprop_step_no_output(self, dL_dh, cache):
         f, i, g, o, c, c_prev = cache
-        dL_do = tanh(c)
+        dL_do = tanh(c)*dL_dh
 
         c_grad = o*tanh_grad(dL_dh, c)
 
-        dL_df = c_grad * c_prev * dL_do
-        dL_di = c_grad * g * dL_do
-        dL_dg = c_grad * i * dL_do
+        dL_df = c_grad * c_prev
+        dL_di = c_grad * g 
+        dL_dg = c_grad * i
         
-        dg_dh = self.backprop_step_linear(dL_dg)
         do_dh = self.backprop_step_linear(dL_do)
+        dg_dh = self.backprop_step_linear(dL_dg)
         di_dh = self.backprop_step_linear(dL_di)
         df_dh = self.backprop_step_linear(dL_df)
 
@@ -104,13 +104,19 @@ class BaseLSTM:
         return dL_dh
 
     def forward_step(self, x, c, h):
+        f,i,g,o = 0, 0,0,0
         f = self.linear_activation_forward(x, h, "f", "sigmoid")
         i = self.linear_activation_forward(x, h, "i", "sigmoid")
-        g = self.linear_activation_forward(x, h, "g", "sigmoid")
-        o = self.linear_activation_forward(x, h, "o", "tanh")
+        g = self.linear_activation_forward(x, h, "g", "tanh")
+        o = self.linear_activation_forward(x, h, "o", "sigmoid")
 
         c_new = f * c + i * g
-        h_new = o * sigmoid(c_new)
+        # c_new = i * g
+        h_new = o * tanh(c_new)
+        # h_new = tanh(c_new)
+
+        # c_new = c
+        # h_new = f
 
         self.save_to_cache((h_new, (f, i, g, o, c_new, c)))
         
@@ -127,7 +133,7 @@ class BaseLSTM:
         if (self.enable_caching):
             self.cache.append(value)
 
-    def forward(self, inp, h=None):
+    def forward(self, inp, h=None, c=None):
         raise NotImplementedError()
     
     def train_on_example(self, x, y, learning_rate):
@@ -150,11 +156,9 @@ class LSTMWithOutput(BaseLSTM):
 
         y = np.dot(LW, x) + Lb
 
-        # y = self.output_activation(y)
-
         self.save_to_cache((LW, Lb, x, y))
 
-        return y
+        return self.output_activation(y)
 
     def backprop_output(self, y):
         LW, Lb, h, y_out = self.cache.pop()
@@ -177,6 +181,7 @@ class LSTMWithOutput(BaseLSTM):
         LW_grad = np.zeros((self.n_dims_out, self.n_dims_hidden))
         Lb_grad = np.zeros((self.n_dims_out, 1))
         self.grads["L"] = (LW_grad, Lb_grad)
+
 
 class ManyToOneLSTM(LSTMWithOutput):
     def forward(self, inp, h=None):